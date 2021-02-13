04fa23e94d35a5f521ee5b91ac7307da4a5a8a0b
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-13 13:10:27 +0800

    Create 1149_Dangerous Goods Packaging (25).cpp
    每放入一个物品，把其和与其不相容的所有物品打上标记true。
    如果某样东西放入时已经被标为true，则这个箱子不可取，输出No
    如果成功放入所有东西，输出Yes

---
+++ Advanced Level/1149_Dangerous Goods Packaging (25).cpp
@@ -0,0 +1,31 @@
+#include<cstdio>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n,m,u,v,i,k;
+    scanf("%d %d",&n,&m);
+    vector<int> check[100010];
+    while(n--){
+        scanf("%d%d",&u,&v);
+        check[u].push_back(v);
+        check[v].push_back(u);
+    }
+    
+    while(m--){
+        scanf("%d",&k);
+        int flag=0;
+        bool container[100010]={false};
+        while(k--){
+            scanf("%d",&u);
+            if(flag==1) continue;
+            if(container[u]) flag=1;
+            for(i=0;i<check[u].size();i++){
+                v=check[u][i];
+                container[v]=true;
+            }
+        }
+        if(flag) printf("No\n");
+        else printf("Yes\n");
+    }
+}
