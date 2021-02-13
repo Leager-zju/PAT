f6e4ba80b0f8006de68534d9e81781dd1c6b9562
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-13 11:48:40 +0800

    Create 1151_LCA in a Binary Tree (30).cpp
    这里是二叉树，而非BST。
    将中序中每个值按大小映射为1,2,3,...，即这些值在先序中的插入顺序。
    最小公共祖先满足：1.插入顺序在这两个数之前 2.值在这两个数之间

---
+++ Advanced Level/1151_LCA in a Binary Tree (30).cpp
@@ -0,0 +1,35 @@
+#include<unordered_map>
+#include<cstdio>
+#include<vector>
+using namespace std;
+
+unordered_map<int,int> repo;
+vector<int> pre;
+
+int main(){
+    int n,m,i,j,u,v;
+    scanf("%d %d",&m,&n);
+    for(i=1;i<=n;i++){
+        scanf("%d",&u);
+        repo[u]=i;
+    }
+    pre.resize(n);
+    for(i=0;i<n;i++){
+        scanf("%d",&pre[i]);
+    }
+    while(m--){
+        scanf("%d %d",&u,&v);
+        if(repo.find(u)==repo.end() && repo.find(v)==repo.end()) printf("ERROR: %d and %d are not found.\n",u,v);
+        else if(repo.find(u)==repo.end()) printf("ERROR: %d is not found.\n",u);
+        else if(repo.find(v)==repo.end()) printf("ERROR: %d is not found.\n",v);
+        else{
+            for(j=0;j<n;j++){
+                i=repo[pre[j]];
+                if( (i==repo[u]) || (i==repo[v]) || (i<=repo[u]&&i>=repo[v]) || (i<=repo[v]&&i>=repo[u]) ) break;
+            }
+            if(i==repo[u]) printf("%d is an ancestor of %d.\n",u,v);
+            else if(i==repo[v]) printf("%d is an ancestor of %d.\n",v,u);
+            else printf("LCA of %d and %d is %d.\n",u,v,pre[j]);
+        }
+    }
+}
