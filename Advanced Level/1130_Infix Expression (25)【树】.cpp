24b6f0ff6f488bee9b5e17dcc2d11e00bdd019f2
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-14 22:41:23 +0800

    Create 1130_Infix Expression (25)【树】.cpp
    中序遍历，对于非叶非根结点要在前后输出括号。

---
+++ Advanced Level/1130_Infix Expression (25)【树】.cpp
@@ -0,0 +1,36 @@
+#include<string>
+#include<iostream>
+using namespace std;
+
+bool ischild[25]={false};
+string ans;
+int root;
+struct node{
+    string data;
+    int left,right;
+}tree[25];
+
+void inorder(int v){
+    if(v!= 1){
+        if(v!=root && (tree[v].left!=-1 || tree[v].right!=-1)) cout<<"(";
+        inorder(tree[v].left);
+        cout<<tree[v].data;
+        inorder(tree[v].right);
+        if(v!=root && (tree[v].left!=-1 || tree[v].right!=-1)) cout<<")";
+    }
+}
+
+int main(){
+    int n,i;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        cin>>tree[i].data>>tree[i].left>>tree[i].right;
+        if(tree[i].left!=-1) ischild[tree[i].left]=true;
+        if(tree[i].right!=-1) ischild[tree[i].right]=true;
+    }
+    for(i=1;i<=n;i++){
+        if(!ischild[i]) break;
+    }
+    root=i;
+    inorder(root);
+}
