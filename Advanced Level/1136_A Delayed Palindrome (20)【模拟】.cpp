c283b76d88c97c6723e4b0c3c32f02f1159e8b4a
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-14 19:01:32 +0800

    Create 1136_A Delayed Palindrome (20)【模拟】.cpp

---
+++ Advanced Level/1136_A Delayed Palindrome (20)【模拟】.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+bool isPalindrome(string s){
+    string temp=s;
+    reverse(s.begin(),s.end());
+    if(temp==s) return true;
+    else return false;
+}
+
+string AplusB(string a,string b){
+    int flag=0;
+    string ans;
+    for(int i=0;i<a.length();i++){
+        int x=a[i]-'0';
+        int y=b[i]-'0';
+        
+        int sum=x+y+flag;
+        if(sum>=10){
+            flag=1;
+            sum-=10;
+        }
+        else{
+            flag=0;
+        }
+        char now=sum+'0';
+        ans.push_back(now);
+    }
+    if(flag) ans.push_back('1');
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
+
+int main(){
+    int count=0;
+    string n,temp;
+    cin>>n;
+    while(!isPalindrome(n) && count<10){
+        temp=n;
+        reverse(temp.begin(),temp.end());
+        cout<<n<<" + "<<temp<<" = ";
+        n=AplusB(n,temp);
+        cout<<n<<endl;
+        count++;
+    }
+    if(count==10) cout<<"Not found in 10 iterations.";
+    else cout<<n<<" is a palindromic number.";
+}
