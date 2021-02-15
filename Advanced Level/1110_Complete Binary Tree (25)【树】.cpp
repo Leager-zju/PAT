25ab09f34fd2ca7ba28057110fdd08a6d07af4f9
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-15 21:50:40 +0800

    Create 1110_Complete Binary Tree (25)【树】.cpp
    判断一棵树是否为完全二叉树。
    如果n是偶数，则判断前n/2-1个结点是否均有左右儿子，第n/2个结点只有左儿子；如果n是奇数，则判断前n/2个结点是否均有左右儿子。

---
+++ Advanced Level/1110_Complete Binary Tree (25)【树】.cpp
@@ -0,0 +1,74 @@
+#include<cstdio>
+#include<string.h>
+#include<math.h>
+#include<queue>
+using namespace std;
+
+struct node{
+    int left,right,id;
+}tree[25];
+
+int isCBT(int root,int n){
+    queue<node> Q;
+    Q.push(tree[root]);
+    int count=1,flag=0;
+    node top;
+    while(!Q.empty()){
+        top=Q.front();
+        Q.pop();
+        if(count<=(n-1)/2 && (top.left==-1 || top.right==-1)){
+            flag=1;
+        }
+        else if(count==n/2 && n%2==0 && (top.left==-1 || top.right!=-1)){
+            flag=1;
+        }
+        if(top.left!=-1) Q.push(tree[top.left]);
+        if(top.right!=-1) Q.push(tree[top.right]);
+        count++;
+    }
+    if(flag) return -1;
+    else return top.id;
+}
+
+int main(){
+    int n,i,root,flag;
+    bool ischild[25]={false};
+    char l[2],r[2];
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%s %s",l,r);
+        tree[i].id=i;
+        if(l[0]=='-') tree[i].left=-1;
+        else{
+            if(strlen(l)==1){
+                tree[i].left=l[0]-'0';
+                ischild[l[0]-'0']=true;
+            }
+            else{
+                tree[i].left=(l[0]-'0')*10+(l[1]-'0');
+                ischild[(l[0]-'0')*10+(l[1]-'0')]=true;
+            }
+        }
+        if(r[0]=='-') tree[i].right=-1;
+        else{
+            if(strlen(r)==1){
+                tree[i].right=r[0]-'0';
+                ischild[r[0]-'0']=true;
+            }
+            else{
+                tree[i].right=(r[0]-'0')*10+(r[1]-'0');
+                ischild[(r[0]-'0')*10+(r[1]-'0')]=true;
+            }
+        }
+    }
+    for(i=0;i<n;i++){
+        if(!ischild[i]){
+            root=i;
+            break;
+        }
+    }
+    
+    flag=isCBT(root,n);
+    if(flag!=-1) printf("YES %d",flag);
+    else printf("NO %d",root);
+}
