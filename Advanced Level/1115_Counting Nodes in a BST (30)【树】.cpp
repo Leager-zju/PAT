7fef6a6bfb97905d945a537ee70add55ee907a3c
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-15 14:23:47 +0800

    Create 1115_Counting Nodes in a BST (30)【树】.cpp
    计算最下面两层的结点数，在建树的时候顺便更新层数就行。

---
+++ Advanced Level/1115_Counting Nodes in a BST (30)【树】.cpp
@@ -0,0 +1,39 @@
+#include<algorithm>
+#include<cstdio>
+using namespace std;
+
+int floor[1010]={0};
+int maxfloor=0;
+
+struct node{
+    node *left=NULL,*right=NULL;
+    int data;
+    node(int x){
+        data=x;
+    }
+};
+void maketree(node *&root,int x,int y){
+    if(root==NULL){
+        root=new node(x);
+        maxfloor=max(maxfloor,y);
+        floor[y]++;
+    }
+    else if(x>root->data) maketree(root->right,x,y+1);
+    else maketree(root->left,x,y+1);
+}
+
+int main(){
+    int n,i;
+    int temp;
+    node *root=NULL;
+    
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++){
+        scanf("%d",&temp);
+        maketree(root,temp,0);
+    }
+
+    if(maxfloor==0) printf("%d + 0 = %d",floor[maxfloor],floor[maxfloor]);
+    else printf("%d + %d = %d",floor[maxfloor],floor[maxfloor-1],floor[maxfloor]+floor[maxfloor-1]);
+}
