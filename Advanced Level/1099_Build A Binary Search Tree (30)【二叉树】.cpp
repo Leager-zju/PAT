d9c0be58f00edd2fa6f512b3c2920a984bf11c19
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-16 12:50:11 +0800

    Create 1099_Build A Binary Search Tree (30)【二叉树】.cpp
    对于BST而言，中序序列即升序序列，反之亦然。利用这个性质对给定结构的BST进行插值，最后层序输出。

---
+++ Advanced Level/1099_Build A Binary Search Tree (30)【二叉树】.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+struct node{
+    int data;
+    int left,right;
+}tree[105];
+
+int list[105],now=0;
+queue<node> levelorder;
+
+void insert(int root){
+    if(tree[root].left!=-1) insert(tree[root].left);
+    tree[root].data=list[now++];
+    if(tree[root].right!=-1) insert(tree[root].right);
+}
+
+int main(){
+    int n,i,first=1;
+    cin>>n;
+    for(i=0;i<n;i++){
+        cin>>tree[i].left>>tree[i].right;
+    }
+    for(i=0;i<n;i++){
+        cin>>list[i];
+    }
+    sort(list,list+i);
+    insert(0);
+    
+    node top;
+    levelorder.push(tree[0]);
+    while(!levelorder.empty()){
+        top=levelorder.front();
+        levelorder.pop();
+        if(first) first=0;
+        else printf(" ");
+        printf("%d",top.data);
+        if(top.left!=-1) levelorder.push(tree[top.left]);
+        if(top.right!=-1) levelorder.push(tree[top.right]);
+    }
+}
