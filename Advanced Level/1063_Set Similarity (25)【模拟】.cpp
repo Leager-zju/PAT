631b71d56c2c840fb3298a83276f48cbd424f7f9
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-16 22:51:43 +0800

    Create 1063_Set Similarity (25)【模拟】.cpp
    set的用法，set.count()返回元素个数。

---
+++ Advanced Level/1063_Set Similarity (25)【模拟】.cpp
@@ -0,0 +1,36 @@
+#include<cstdio>
+#include<unordered_set>
+#include<algorithm>
+using namespace std;
+
+void compare(unordered_set<int> a,unordered_set<int> b){
+    double nc=0,nt=(double)b.size(),similar;
+    int num;
+    for(auto it=a.begin();it!=a.end();it++){
+        num=*it;
+        if(b.count(num)!=0) nc++;
+        else nt++;
+    }
+    similar=nc/nt*100.0;
+    printf("%.1f%%\n",similar);
+}
+
+int main(){
+    int n,m,num;
+    int i,j;
+    scanf("%d",&n);
+    
+    unordered_set<int> s[n+1];
+    for(i=1;i<=n;i++){
+        scanf("%d",&m);
+        for(j=0;j<m;j++){
+            scanf("%d",&num);
+            s[i].insert(num);
+        }
+    }
+    scanf("%d",&num);
+    for(i=0;i<num;i++){
+        scanf("%d %d",&n,&m);
+        compare(s[n],s[m]);
+    }
+}
