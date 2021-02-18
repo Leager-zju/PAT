f51b7d72aa037f2ae098158abbb272171dcac932
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-18 16:28:58 +0800

    Update 1150_Travelling Salesman Problem (25)【图】.cpp
    TS cycle: 每个点都去过，且是环路
    TS simple cycle: 在上面基础上，每个点仅出现一次
    对于任意路径，只要有点没去过，或者两点之间路不通，或者起点终点不同，则 Not a TS cycle

--- Advanced Level/1150_Travelling Salesman Problem (25)【图】.cpp
+++ Advanced Level/1150_Travelling Salesman Problem (25)【图】.cpp
@@ -14,43 +14,38 @@ int main(){
     }
     cin>>m;
     for(j=1;j<=m;j++){
-        // j号path
         int flag=0,totaldis=0,start;
         bool vis[n+1]={false};
         cin>>k>>u;
         start=u;
         for(i=1;i<k;i++){
             cin>>v;
-            if(flag==1) continue;
-            if(graph[u][v]==0){ // 两点之间没路
+            if(flag) continue;
+            if(graph[u][v]==0){
                 flag=1;
                 totaldis=0;
                 continue;
             }
             else totaldis+=graph[u][v];
-            
             if(!vis[v]) vis[v]=true;
-            else flag=2;        // 访问了已经访问的点
+            else flag=2;
             u=v;
         }
         cout<<"Path "<<j<<": ";
         if(totaldis==0) cout<<"NA";
         else cout<<totaldis;
-        
-        // 如果有点没访问或起点终点不同
+
         if(v!=start) flag=1;
         for(i=1;i<=n;i++){
-            if(!vis[i]){        
+            if(!vis[i]){
                 flag=1;
                 break;
             }
         }
-        
         if(flag==0)      cout<<" (TS simple cycle)"<<endl;
         else if(flag==1) cout<<" (Not a TS cycle)"<<endl;
         else if(flag==2) cout<<" (TS cycle)"<<endl;
         
-        // 如果是TS cycle/TS simple cycle更新最短路径编号及长度
         if(flag!=1 && shortest>totaldis){
             shortest=totaldis;
             id=j;
