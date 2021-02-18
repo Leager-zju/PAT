d32b4dd03f38dd6e33917c65814ebf4b8a55a94d
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-18 19:38:32 +0800

    Update 1141_PAT Ranking of Institutions (25)【模拟】.cpp
    常规数据结构排序题

--- Advanced Level/1141_PAT Ranking of Institutions (25)【模拟】.cpp
+++ Advanced Level/1141_PAT Ranking of Institutions (25)【模拟】.cpp
@@ -1,62 +1,53 @@
 #include<string>
+#include<vector>
+#include<algorithm>
 #include<unordered_map>
 #include<iostream>
-#include<algorithm>
-#include<vector>
+#include<cstdio>
 using namespace std;
 
 struct info{
-    string schoolname;
-    int B,A,T;
-    int tws,ns;
-}temp;
-unordered_map<string,info> m;
+    string name;
+    int B=0,A=0,T=0;
+    int tws=0,Ns=0;
+};
+
+bool cmp(info &a,info &b){
+    if(a.tws!=b.tws) return a.tws>b.tws;
+    else if(a.Ns!=b.Ns) return a.Ns<b.Ns;
+    else return a.name<b.name;
+}
+
+unordered_map<string,info> list;
 vector<info> ranklist;
 
-string lower(string s){
-    string ans;
-    for(int i=0;i<s.length();i++){
-        ans+=tolower(s[i]);
-    }
-    return ans;
-}
-bool cmp(info a,info b){
-    if(a.tws!=b.tws) return a.tws>b.tws;
-    else if(a.ns!=b.ns) return a.ns<b.ns;
-    else return a.schoolname<b.schoolname;
-}
-
 int main(){
-    int n,i,grade;
-    string id,school;
-    cin>>n;
-    while(n--){
-        cin>>id>>grade>>school;
-        school=lower(school);
-        if(m.find(school)==m.end()){
-            if(id[0]=='B') m[school]=info({school,grade,0,0,0,1});
-            else if(id[0]=='A') m[school]=info({school,0,grade,0,0,1});
-            else m[school]=info({school,0,0,grade,0,1});
+    int n,i,j,cnt=1;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        char id[6];
+        int grade;
+        string school;
+        scanf("%s %d",id,&grade);
+        cin>>school;
+        for(j=0;j<school.length();j++){
+            school[j]=tolower(school[j]);
         }
-        else{
-            if(id[0]=='B') m[school].B+=grade;
-            else if(id[0]=='T') m[school].T+=grade;
-            else m[school].A+=grade;
-            m[school].ns++;
-        }
+        if(id[0]=='B') list[school].B+=grade;
+        else if(id[0]=='A') list[school].A+=grade;
+        else if(id[0]=='T') list[school].T+=grade;
+        list[school].Ns++;
     }
-    for(auto it=m.begin();it!=m.end();it++){
-        temp=it->second;
-        temp.tws=temp.B/1.5+temp.A+temp.T*1.5;
-        ranklist.push_back(temp);
+    for(auto it=list.begin();it!=list.end();it++){
+        it->second.tws=it->second.B/1.5+it->second.A+it->second.T*1.5;
+        it->second.name=it->first;
+        ranklist.push_back(it->second);
     }
     sort(ranklist.begin(),ranklist.end(),cmp);
-    int rank=1;
-    cout<<(int)ranklist.size()<<endl;
+    
+    printf("%d\n",(int)ranklist.size()); 
     for(i=0;i<ranklist.size();i++){
-        if(i!=0 && ranklist[i].tws!=ranklist[i-1].tws){
-            rank=i+1;
-        }
-        cout<<rank<<" "<<ranklist[i].schoolname<<" "<<ranklist[i].tws<<" "<<ranklist[i].ns<<endl;
+        if(i>0 && ranklist[i].tws!=ranklist[i-1].tws) cnt=i+1;
+        printf("%d %s %d %d\n",cnt,ranklist[i].name.c_str(),ranklist[i].tws,ranklist[i].Ns);
     }
 }
