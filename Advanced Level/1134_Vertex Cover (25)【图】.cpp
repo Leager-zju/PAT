01d812268874b019b34fe7947565b7fa1c64cce8
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-18 21:27:16 +0800

    Update 1134_Vertex Cover (25)【图】.cpp
    将边编号，每访问一个结点，将其所有邻边标true，检查标记的边数是否与总边数相等。

--- Advanced Level/1134_Vertex Cover (25)【图】.cpp
+++ Advanced Level/1134_Vertex Cover (25)【图】.cpp
@@ -4,25 +4,21 @@
 using namespace std;
 
 int main(){
-    int v,e;    // 点数(0~v-1)，边数(0~e-1)
+    int v,e,i,j;
     int v1,v2,Q,k;
-    int i,j;
+    vector<int> EdgeOfVertex[10010];
+    
     cin>>v>>e;
-    
-    vector<int> EdgeOfVertex[v];
-    bool cover[e];
-    
     for(i=0;i<e;i++){
         cin>>v1>>v2;
         EdgeOfVertex[v1].push_back(i);
         EdgeOfVertex[v2].push_back(i);
     }
-    
     cin>>Q;
     while(Q--){
         cin>>k;
         int count=0;
-        fill(cover,cover+e,false);
+        bool cover[e]={false};
         while(k--){
             cin>>v;
             for(i=0;i<EdgeOfVertex[v].size();i++){
