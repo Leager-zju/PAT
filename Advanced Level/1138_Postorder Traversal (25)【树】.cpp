e6b5757b1c69140f0202b8b1a2dc7773b8bd8cf8
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-18 20:16:39 +0800

    Update 1138_Postorder Traversal (25)【树】.cpp
    如果结点有左子树，往左搜索；反之往右搜索。直至叶结点。

--- Advanced Level/1138_Postorder Traversal (25)【树】.cpp
+++ Advanced Level/1138_Postorder Traversal (25)【树】.cpp
@@ -1,40 +1,27 @@
-#include<iostream>
-using namespace std;
+#include<cstdio>
+#define maxn 50010
 
-int in[50010],pre[50010];
-struct node{
-    int data;
-    node *left=NULL,*right=NULL;
-    node(int x){
-        data=x;
-    }
-};
-node *maketree(int l1,int r1,int l2,int r2){
-    if(l1>r1) return NULL;
+int in[maxn],pre[maxn];
+
+int find(int l1,int r1,int l2,int r2){
+    if(l1==r1) return pre[l1];
     int i,numofleft;
     for(i=l2;i<=r2;i++){
         if(in[i]==pre[l1]) break;
     }
     numofleft=i-l2;
-    node *root=new node(pre[l1]);
-    root->left=maketree(l1+1,l1+numofleft,l2,i-1);
-    root->right=maketree(l1+numofleft+1,r1,i+1,r2);
-    return root;
+    if(numofleft) return find(l1+1,l1+numofleft,l2,i-1);
+    else return find(l1+numofleft+1,r1,i+1,r2);
 }
 
 int main(){
     int n,i;
-    cin>>n;
+    scanf("%d",&n);
     for(i=0;i<n;i++){
-        cin>>pre[i];
+        scanf("%d",&pre[i]);
     }
     for(i=0;i<n;i++){
-        cin>>in[i];
+        scanf("%d",&in[i]);
     }
-    node *root=maketree(0,n-1,0,n-1);
-    while(root->left || root->right){
-        if(root->left) root=root->left;
-        else root=root->right;
-    }
-    cout<<root->data;
+    printf("%d",find(0,n-1,0,n-1));
 }
