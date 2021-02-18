77ad1fd996612b898e4a26e2d604764750399e61
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-18 16:46:00 +0800

    Update 1147_Heaps (30)【二叉树】.cpp
    常规

--- Advanced Level/1147_Heaps (30)【二叉树】.cpp
+++ Advanced Level/1147_Heaps (30)【二叉树】.cpp
@@ -2,11 +2,13 @@
 #include<cstdio>
 using namespace std;
 
-void postorder(vector<int> &list,int i){
-    if(i<list.size()){
-        postorder(list,i*2);    // 访问左子树
-        postorder(list,i*2+1);  // 访问右子树
-        printf("%d",list[i]);
+vector<int> list;
+
+void postorder(int root){
+    if(root<list.size()){
+        postorder(root*2);
+        postorder(root*2+1);
+        printf("%d",list[root]);
         if(i==1) printf("\n");
         else printf(" ");
     }
@@ -15,19 +17,17 @@ void postorder(vector<int> &list,int i){
 int main(){
     int n,m,i;
     scanf("%d %d",&m,&n);
+    list.resize(n+1);
     while(m--){
-        vector<int> list(n+1);
         int ismax=1,ismin=1;
-        scanf("%d",&list[1]);
-        for(i=2;i<=n;i++){
+        for(i=1;i<=n;i++){
             scanf("%d",&list[i]);
-            if(list[i]>list[i/2]) ismax=0;
-            if(list[i]<list[i/2]) ismin=0;
+            if(i>1 && list[i]>list[i/2]) ismax=0;
+            if(i>1 && list[i]<list[i/2]) ismin=0;
         }
-        if(ismax==1) printf("Max Heap\n");
-        else if(ismin==1) printf("Min Heap\n");
-        else printf("Not Heap\n");
-        
-        postorder(list,1);
+        if(ismax)       printf("Max Heap\n");
+        else if(ismin)  printf("Min Heap\n");
+        else            printf("Not Heap\n");
+        postorder(1);
     }
 }
