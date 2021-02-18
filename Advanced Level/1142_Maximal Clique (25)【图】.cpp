51450bf981a00baeee76578853693f00843a88c7
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-18 19:19:42 +0800

    Update 1142_Maximal Clique (25)【图】.cpp
    先判断给定的集合是否构成完全图，再在剩下的点中寻找是否有点与集合中所有点相邻。

--- Advanced Level/1142_Maximal Clique (25)【图】.cpp
+++ Advanced Level/1142_Maximal Clique (25)【图】.cpp
@@ -2,49 +2,45 @@
 #include<vector>
 using namespace std;
 
+bool Graph[202][202]={false};
+
 int main(){
-    int Graph[202][202]={0};
     int n,m,k,i,j;
     int v1,v2;
     scanf("%d %d",&n,&m);
     for(i=0;i<m;i++){
         scanf("%d %d",&v1,&v2);
-        Graph[v1][v2]=1;
-        Graph[v2][v1]=1;
+        Graph[v1][v2]=Graph[v2][v1]=true;
     }
     scanf("%d",&m);
     while(m--){
         int flag=0;
-        scanf("%d",&k);
-        
-        vector<int> temp;
         bool vis[n+1]={false};
-
+        vector<int> temp;
+        scanf("%d",&k);
         for(i=0;i<k;i++){
             scanf("%d",&v1);
             if(flag) continue;
             for(j=0;j<temp.size();j++){
-                if(Graph[v1][temp[j]]==0) flag=1;
+                if(!Graph[v1][temp[j]]) flag=1;
             }
             temp.push_back(v1);
             vis[v1]=true;
         }
-        
         if(flag) printf("Not a Clique\n");
         else{
-            int ans=1;
             for(i=1;i<=n;i++){
                 if(vis[i]) continue;
                 for(j=0;j<temp.size();j++){
-                    if(Graph[i][temp[j]]==0) break;
+                    if(!Graph[i][temp[j]]) break;
                 }
                 if(j==temp.size()){
-                    ans=0;
+                    flag=1;
                     printf("Not Maximal\n");
                     break;
                 }
             }
-            if(ans) printf("Yes\n");
+            if(!flag) printf("Yes\n");
         }
     }
 }
