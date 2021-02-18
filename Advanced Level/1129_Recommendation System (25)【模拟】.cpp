004b6cc7364d1a274be1760904fce0e56d234137
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-18 22:35:17 +0800

    Update 1129_Recommendation System (25)【模拟】.cpp
    set的操作与运算符重载。
    用times[]存放每个元素出现的次数。
    这里借鉴了柳神的代码。。原来的原创代码测试时间稳定在200ms，还是柳神更的快一点。

--- Advanced Level/1129_Recommendation System (25)【模拟】.cpp
+++ Advanced Level/1129_Recommendation System (25)【模拟】.cpp
@@ -1,37 +1,35 @@
 #include<set>
-#include<algorithm>
 #include<cstdio>
 using namespace std;
-#define maxn 50002
+#define maxn 50010
 
+struct node{
+    int data,count;
+    bool operator < (const node &a) const{
+        if(this->count!=a.count) return this->count>a.count;
+        else return this->data<a.data;
+    }
+};
+set<node> recommend;
 int times[maxn]={0};
 
 int main(){
-    int n,k,i,temp,maxtime=1;
+    int n,k,i,num;
     scanf("%d %d",&n,&k);
-    set<int> recommend[n+1];
-    
-    scanf("%d",&temp);
-    times[temp]=1;
-    recommend[1].insert(temp);
-    
-    for(i=1;i<n;i++){
-        scanf("%d",&temp);
-        printf("%d:",temp);
-        
-        int count=min(i,k),m=maxtime;
-        while(m){
-            for(auto it=recommend[m].begin();it!=recommend[m].end() && count;it++){
-                printf(" %d",*(it));
+    for(i=0;i<n;i++){
+        scanf("%d:",&num);
+        if(i!=0){
+            int count=k;
+            printf("%d:",num);
+            for(auto it=recommend.begin();it!=recommend.end() && count;it++){
+                printf(" %d",(*it).data);
                 count--;
             }
-            m--;
+            printf("\n");
         }
-        printf("\n");
-
-        recommend[times[temp]].erase(temp);
-        times[temp]++;
-        recommend[times[temp]].insert(temp);
-        maxtime=max(maxtime,times[temp]);
+        auto it=recommend.find({num,times[num]});
+        if(it!=recommend.end()) recommend.erase(it);
+        times[num]++;
+        recommend.insert({num,times[num]});
     }
 }
