716b033cfa6a80009bf9d9c678ae008ce0faf21f
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-18 20:02:21 +0800

    Update 1139_First Contact (30 分)【模拟】.cpp
    对给定的两个人，分别找他们的同性朋友，如果两个朋友还刚好认识，那整挺好。

--- Advanced Level/1139_First Contact (30 分)【模拟】.cpp
+++ Advanced Level/1139_First Contact (30 分)【模拟】.cpp
@@ -7,32 +7,28 @@ using namespace std;
 #define maxn 10010
 
 struct info{
-    int id;
-    int gender;     //1男 -1女
-}p1,p2;
-struct Pair{
-    int u1,u2;
-}temp;
+    int id,gender;
+}p1,p2,temp;
 
 vector<info> relation[maxn];
-int G[maxn][maxn]={0};
+bool G[maxn][maxn]={false};
 
-bool cmp(Pair a,Pair b){
-    if(a.u1!=b.u1) return a.u1<b.u1;
-    else return a.u2<b.u2;
+bool cmp(info &a,info &b){
+    if(a.id!=b.id) return a.id<b.id;
+    else return a.gender<b.gender;
 }
 int strtonum(string s){
-    int ans=0;
-    for(int i=0;i<s.length();i++){
+    int i,ans=0;
+    for(i=0;i<s.length();i++){
         ans=ans*10+(s[i]-'0');
     }
     return ans;
 }
 
 int main(){
-    int n,m,q,cnt=0;
+    int n,m,q,i,j;
     string a,b;
-    cin>>n>>m;
+    scanf("%d %d",&n,&m);
     while(m--){
         cin>>a>>b;
         if(a[0]=='-'){
@@ -40,45 +36,41 @@ int main(){
             p1.gender=-1;
         }
         else p1.gender=1;
-        p1.id=strtonum(a);
         if(b[0]=='-'){
             b=b.substr(1,4);
             p2.gender=-1;
         }
         else p2.gender=1;
+        
+        p1.id=strtonum(a);
         p2.id=strtonum(b);
         if(p1.gender==p2.gender){
             relation[p1.id].push_back(p2);
             relation[p2.id].push_back(p1);
         }
-        G[p1.id][p2.id]=G[p2.id][p1.id]=1;
+        G[p1.id][p2.id]=G[p2.id][p1.id]=true;
     }
     
-    cin>>q;
+    scanf("%d",&q);
     while(q--){
-        vector<Pair> ans;
+        vector<info> ans;
         cin>>a>>b;
         if(a[0]=='-') a=a.substr(1,4);
         if(b[0]=='-') b=b.substr(1,4);
         n=strtonum(a);
         m=strtonum(b);
         
-        for(int i=0;i<relation[n].size();i++){
+        for(i=0;i<relation[n].size();i++){
             if(relation[n][i].id==m) continue;
-            for(int j=0;j<relation[m].size();j++){
+            for(j=0;j<relation[m].size();j++){
                 if(relation[m][j].id==n) continue;
-                temp.u1=relation[n][i].id;
-                temp.u2=relation[m][j].id;
-                if(G[temp.u1][temp.u2]){
-                    ans.push_back(temp);
-                }
+                if(G[relation[n][i].id][relation[m][j].id]) ans.push_back({relation[n][i].id,relation[m][j].id});
             }
         }
-        
-        cout<<(int)ans.size()<<endl;
+        printf("%d\n",(int)ans.size());
         sort(ans.begin(),ans.end(),cmp);
-        for(int i=0;i<ans.size();i++){
-            printf("%04d %04d\n",ans[i].u1,ans[i].u2);
+        for(i=0;i<ans.size();i++){
+            printf("%04d %04d\n",ans[i].id,ans[i].gender);
         }
     }
 }
