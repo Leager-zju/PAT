97eb55b97c06508ad9cb73cf4e13bce9a802633f
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-18 22:41:39 +0800

    Update 1128_N Queens Puzzle (20)【数组】.cpp
    边输入边check

--- Advanced Level/1128_N Queens Puzzle (20)【数组】.cpp
+++ Advanced Level/1128_N Queens Puzzle (20)【数组】.cpp
@@ -2,23 +2,16 @@
 #include<math.h>
 
 int main(){
-    int n,k,i,j,flag;
-    int board[n+1];
-    
+    int n,k,i,j;
     scanf("%d",&k);
     while(k--){
-        flag=0;
         scanf("%d",&n);
+        int board[n+1],flag=0;
         for(i=1;i<=n;i++){
             scanf("%d",&board[i]);
-        }
-        for(i=1;i<=n-1;i++){
-            if(flag) break;
-            for(j=i+1;j<=n;j++){
-                if(j-i==abs(board[j]-board[i]) || board[i]==board[j]){
-                    flag=1;
-                    break;
-                }
+            if(flag) continue;
+            for(j=1;j<i;j++){
+                if(i-j==abs(board[i]-board[j]) || board[i]==board[j]) flag=1;
             }
         }
         if(flag) printf("NO\n");
