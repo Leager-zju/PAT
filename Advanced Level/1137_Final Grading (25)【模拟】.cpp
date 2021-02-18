36fba1cf840543c0d5594928f108393fb905606a
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-18 20:32:34 +0800

    Update 1137_Final Grading (25)【模拟】.cpp
    round()函数可以四舍五入。

--- Advanced Level/1137_Final Grading (25)【模拟】.cpp
+++ Advanced Level/1137_Final Grading (25)【模拟】.cpp
@@ -8,11 +8,10 @@ using namespace std;
 
 struct info{
     string name;
-    int Gp,Gm,Gf;
-    int G;
+    int Gp=-1,Gm=-1,Gf=-1,G;
 }stu;
 
-bool cmp(info a,info b){
+bool cmp(info &a,info &b){
     if(a.G!=b.G) return a.G>b.G;
     else return a.name<b.name;
 }
@@ -22,34 +21,30 @@ int main(){
     vector<info> rank;
     string name;
     int n,m,k,grade;
-    
-    cin>>n>>m>>k;
+    scanf("%d %d %d",&n,&m,&k);
     while(n--){
         cin>>name>>grade;
         if(grade>=200){
-            list[name]=info({name,grade,-1,-1,-1});
+            list[name].Gp=grade;
         }
     }
     while(m--){
         cin>>name>>grade;
-        if(list.find(name)!=list.end()){
-            list[name].Gm=grade;
-        }
+        if(list.find(name)!=list.end()) list[name].Gm=grade;
     }
     while(k--){
         cin>>name>>grade;
-        if(list.find(name)!=list.end()){
-            list[name].Gf=grade;
-        }
+        if(list.find(name)!=list.end()) list[name].Gf=grade;
     }
     for(auto it=list.begin();it!=list.end();it++){
         stu=it->second;
+        stu.name=it->first;
         if(stu.Gf>=stu.Gm) stu.G=stu.Gf;
         else stu.G=round(0.4*stu.Gm+0.6*stu.Gf);
         if(stu.G>=60) rank.push_back(stu);
     }
     sort(rank.begin(),rank.end(),cmp);
     for(int i=0;i<rank.size();i++){
-        cout<<rank[i].name<<" "<<rank[i].Gp<<" "<<rank[i].Gm<<" "<<rank[i].Gf<<" "<<rank[i].G<<endl;
+        printf("%s %d %d %d %d\n",rank[i].name.c_str(),rank[i].Gp,rank[i].Gm,rank[i].Gf,rank[i].G);
     }
 }
