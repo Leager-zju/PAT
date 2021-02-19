2b02afc67bb8929f77daf0de24355f0fade73e25
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-19 15:18:57 +0800

    Update 1121_Damn Single (25)【模拟】.cpp
    只有非单身且另一半到场才不会被认为是damn single

--- Advanced Level/1121_Damn Single (25)【模拟】.cpp
+++ Advanced Level/1121_Damn Single (25)【模拟】.cpp
@@ -5,36 +5,29 @@ using namespace std;
 #define maxn 100010
 
 bool present[maxn]={false};
-int partner[maxn];
+vector<int> partner(maxn,-1);
 
 int main(){
-    int n,m,i,count=0;
-    int a,b;
+    int n,m,i,a,b;
     vector<int> participant,damn_single;
     
     scanf("%d",&n);
-    fill(partner,partner+maxn,-1);
     while(n--){
         scanf("%d %d",&a,&b);
         partner[a]=b;
         partner[b]=a;
     }
-    
     scanf("%d",&m);
     while(m--){
         scanf("%d",&a);
         present[a]=true;
         participant.push_back(a);
     }
-    
     for(i=0;i<participant.size();i++){
         int other=partner[participant[i]];
-        if(other==-1 || !present[other]){
-            count++;
-            damn_single.push_back(participant[i]);
-        }
+        if(other==-1 || !present[other]) damn_single.push_back(participant[i]);
     }
-    printf("%d\n",count);
+    printf("%d\n",(int)damn_single.size());
     sort(damn_single.begin(),damn_single.end());
     for(i=0;i<damn_single.size();i++){
         if(i!=0) printf(" ");
