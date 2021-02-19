16ce6d5aa2af5d52ebdfb87bbd6c9e524b6a0ee3
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-19 16:34:34 +0800

    Update 1113_Integer Set Partition (25)【模拟】.cpp
    简单，数学问题。
    前最小的n/2个元素作为第一个集合的元素，剩下的作为第二个集合的元素即可。

--- Advanced Level/1113_Integer Set Partition (25)【模拟】.cpp
+++ Advanced Level/1113_Integer Set Partition (25)【模拟】.cpp
@@ -3,20 +3,16 @@
 using namespace std;
 
 int main(){
-    int n,i,m;
+    int n,i;
     long int num[100010],sum1=0,sum=0;
     scanf("%d",&n);
     for(i=0;i<n;i++){
         scanf("%ld",&num[i]);
-        
+        sum+=num[i];
     }
     sort(num,num+n);
-    m=n/2;
-    for(i=0;i<m;i++){
+    for(i=0;i<n/2;i++){
         sum1+=num[i];
     }
-    for(i=m;i<n;i++){
-        sum2+=num[i];
-    }
-    printf("%d %ld",n-2*m,sum2-sum1);
+    printf("%d %ld",n%2,sum-2*sum1);
 }
