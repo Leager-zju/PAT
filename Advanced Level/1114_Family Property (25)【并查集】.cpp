2852fa327f341f7ab70f93fa77dcd19e9c19eda7
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-19 16:29:36 +0800

    Update 1114_Family Property (25)【并查集】.cpp
    较复杂……思路是对于每行输入，把成员合并，并把所有出现过的成员放在一个list中，之后遍历list，更新其father（即题中族长）的房产数、总面积和总人数，并开一个vector存放各个族长，最后根据平均房产面积sort并输出。

--- Advanced Level/1114_Family Property (25)【并查集】.cpp
+++ Advanced Level/1114_Family Property (25)【并查集】.cpp
@@ -1,3 +1,4 @@
+// 并查集
 #include<cstdio>
 #include<vector>
 #include<unordered_set>
@@ -6,13 +7,12 @@ using namespace std;
 
 struct info{
     int id;
-    int set=0,area=0;
-    int members=0;
+    int numofset=0,area=0,members=0;
 }property[10010];
 
 bool cmp(int a,int b){
     if(1.0*property[a].area/property[a].members!=1.0*property[b].area/property[b].members) return 1.0*property[a].area/property[a].members>1.0*property[b].area/property[b].members;
-    else return property[a].id<property[b].id;
+    else return a<b;
 }
 
 int father[10000];
@@ -38,13 +38,13 @@ void merge(int x,int y){
 }
 
 int main(){
-    int n,i,j,k,id,fa,mo;
+    int n,i,j,k;
+    int id,fa,mo,count=0;
     vector<int> family;
     unordered_set<int> list;
     bool flag[10010]={false};
     
     scanf("%d",&n);
-
     for(i=0;i<10000;i++){
         father[i]=i;
         property[i].id=i;
@@ -67,14 +67,13 @@ int main(){
             list.insert(child);
             merge(id,child);
         }
-        scanf("%d %d",&property[id].set,&property[id].area);
+        scanf("%d %d",&property[id].numofset,&property[id].area);
     }
-    int count=0;
-    for(auto it=list.begin();it!=list.end();it++){
-        i=*(it);
+
+    for(auto i:list){
         int x=findfather(i);
         if(i!=x){
-            property[x].set+=property[i].set;
+            property[x].numofset+=property[i].numofset;
             property[x].area+=property[i].area;
         }
         property[x].members++;
@@ -84,12 +83,11 @@ int main(){
             count++;
         }
     }
-    
-    sort(family.begin(),family.end(),cmp);
     printf("%d\n",count);
+    sort(family.begin(),family.end(),cmp);
     for(i=0;i<family.size();i++){
         id=family[i];
         printf("%04d %d ",id,property[id].members);
-        printf("%.3f %.3f\n",1.0*property[id].set/property[id].members,1.0*property[id].area/property[id].members);
+        printf("%.3f %.3f\n",1.0*property[id].numofset/property[id].members,1.0*property[id].area/property[id].members);
     }
 }
