f6118f16b0623eae9846ab42adda3208642d8bfd
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-19 16:44:50 +0800

    Update 1112_Stucked Keyboard (20)【字符串】.cpp
    第一遍找出问题的键：如果一个字符并不重复出现k次，标false；反之若重复出现k次且未被标false，标true
    第二遍把字符串中所有出问题的键的后k-1个字符删去
    第三遍遍历字符串，如果某个字符是true，输出它并改为false
    最后输出字符串

--- Advanced Level/1112_Stucked Keyboard (20)【字符串】.cpp
+++ Advanced Level/1112_Stucked Keyboard (20)【字符串】.cpp
@@ -1,6 +1,5 @@
 #include<string>
 #include<unordered_map>
-#include<vector>
 #include<algorithm>
 #include<iostream>
 using namespace std;
@@ -9,33 +8,24 @@ int main(){
     int i=0,j,k;
     string input;
     unordered_map<char,bool> stuck;
-    vector<char> ans;
     
-    cin>>k;
-    cin>>input;
+    cin>>k>>input;
     while(i<=input.length()-k){
         for(j=i+1;j<i+k;j++){
             if(input[j]!=input[i]) break;
         }
         if(j!=i+k) stuck[input[i]]=false;
-        else if(stuck.find(input[i])==stuck.end()){
-            stuck[input[i]]=true;
-
+        else if(stuck.find(input[i])==stuck.end()) stuck[input[i]]=true;
         i=j;
     }
     for(i=0;i<input.length();i++){
-        if(stuck[input[i]]){
-            input.erase(input.begin()+i+1,input.begin()+i+k);
-        }
+        if(stuck[input[i]]) input.erase(input.begin()+i+1,input.begin()+i+k);
     }
     for(i=0;i<input.length();i++){
         if(stuck[input[i]]){
-            ans.push_back(input[i]);
+            cout<<input[i];
             stuck[input[i]]=false;
         }
-    }
-    for(i=0;i<ans.size();i++){
-        cout<<ans[i];
     }
     cout<<endl<<input;
 }
