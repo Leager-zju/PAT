414b5f12588cd26b905e6c989d0ef8562b6757e4
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-19 23:15:01 +0800

    Update 1106_Lowest Price in Supply Chain (25)【树】.cpp
    BFS，DFS都行

--- Advanced Level/1106_Lowest Price in Supply Chain (25)【树】.cpp
+++ Advanced Level/1106_Lowest Price in Supply Chain (25)【树】.cpp
@@ -1,51 +1,46 @@
-#include<iostream>
 #include<cstdio>
 #include<queue>
 #include<vector>
-#include<algorithm>
 using namespace std;
+#define maxn 100010
 
 int main(){
-    int n,k,id,i,j,count=1,minfloor=10e7;
     double p,r;
-    
-    cin>>n>>p>>r;
-    r=(1+r/100);
-    
-    vector<int> next[n];
+    int n,k,id,i;
+    int count=1,minfloor=10e7;
+    int floor[maxn];
     queue<int> Q;
-    int floor[n];
-    floor[0]=0;
+    vector<int> next[maxn];
     
+    scanf("%d %lf %lf",&n,&p,&r);
     for(i=0;i<n;i++){
-        cin>>k;
-        for(j=0;j<k;j++){
-            cin>>id;
+        scanf("%d",&k);
+        while(k--){
+            scanf("%d",&id);
             next[i].push_back(id);
         }
     }
+    
     Q.push(0);
+    floor[0]=0;
     while(!Q.empty()){
         id=Q.front();
         Q.pop();
         for(i=0;i<next[id].size();i++){
-            j=next[id][i];
-            Q.push(j);
-            floor[j]=floor[id]+1;
-            if(next[j].size()==0){
-                if(floor[j]<minfloor){
-                    minfloor=floor[j];
-                    count=1;
-                }
-                else if(floor[j]==minfloor){
-                    count++;
-                }
+            Q.push(next[id][i]);
+            floor[next[id][i]]=floor[id]+1;
+        }
+        if(next[id].size()==0){
+            if(floor[id]<minfloor){
+                minfloor=floor[id];
+                count=1;
             }
+            else if(floor[id]==minfloor) count++;
         }
     }
-    if(n==1) minfloor=0;
-    for(i=0;i<minfloor;i++){
+    r=(1+r/100);
+    while(minfloor--){
         p*=r;
     }
-    printf("%.4f %d",p,count);
+    printf("%.4f %d\n",p,count);
 }
