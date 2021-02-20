a9466eb1a334f391134a5e8c0348d4714e6060d6
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-20 18:27:46 +0800

    Update 1071_Speech Patterns (25)【字符串】.cpp
    insensitive:小写的

--- Advanced Level/1071_Speech Patterns (25)【字符串】.cpp
+++ Advanced Level/1071_Speech Patterns (25)【字符串】.cpp
@@ -1,17 +1,17 @@
-#include<map>
+#include<unordered_map>
 #include<string>
 #include<cstdio>
 using namespace std;
 
 int main(){
     string word,ans;
-    map<string,int> list;
+    unordered_map<string,int> list;
     char c;
     int maxtime=0;
     while(1){
         c=getchar();
-        if(isalpha(c)) word+=tolower(c);
-        else if(isdigit(c)) word+=c;
+        if(isalpha(c)) word.push_back(tolower(c));
+        else if(isdigit(c)) word.push_back(c);
         else if(word.length()!=0){
             list[word]++;
             if(maxtime<list[word]){
