bb0d916e22311ccb3715c961f1b1b72dcb3cd88a
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-20 12:28:51 +0800

    Update 1094_The Largest Generation (25)【树】.cpp
    常规层序遍历题

--- Advanced Level/1094_The Largest Generation (25)【树】.cpp
+++ Advanced Level/1094_The Largest Generation (25)【树】.cpp
@@ -8,12 +8,11 @@ struct info{
     vector<int> child;
 }member[110],top;
 
-
-
 int main(){
-    int n,m;
-    int i,j,father,num,maxg=1,maxi=1,son;
+    int n,m,i,j,father,son,num;
+    int maxg=1,maxi=1;
     int generation[110]={0};
+    
     scanf("%d %d",&n,&m);
     for(i=0;i<m;i++){
         scanf("%d %d",&father,&num);
@@ -25,22 +24,20 @@ int main(){
     
     queue<info> Q;
     member[1].floor=1;
-    generation[1]=1;
     Q.push(member[1]);
     while(!Q.empty()){
         top=Q.front();
         Q.pop();
+        generation[top.floor]++;
+        if(generation[top.floor]>maxg){
+            maxg=generation[top.floor];
+            maxi=top.floor;
+        }
         for(i=0;i<top.child.size();i++){
             int u=top.child[i];
             member[u].floor=top.floor+1;
-            j=member[u].floor;
-            generation[j]++;
-            if(generation[j]>maxg){
-                maxg=generation[j];
-                maxi=j;
-            }
             Q.push(member[u]);
         }
     }
-    printf("%d %d",maxg,maxi);
+    printf("%d %d\n",maxg,maxi);
 }
