51dcf8172daa30658005c3f8e756b2e722b6b001
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-20 17:37:23 +0800

    Update 1074_Reversing Linked List (25)【链表】.cpp
    常规题

--- Advanced Level/1074_Reversing Linked List (25)【链表】.cpp
+++ Advanced Level/1074_Reversing Linked List (25)【链表】.cpp
@@ -17,10 +17,6 @@ int main(){
         scanf("%d %d",&l[addr].data,&l[addr].next);
         l[addr].address=addr;
     }
-    if(head==-1){
-        printf("-1");
-        return 0;
-    }
     while(head!=-1){
         list.push_back(l[head]);
         head=l[head].next;
@@ -29,9 +25,9 @@ int main(){
     for(i=0;i<=len-k;i+=k){
         reverse(list.begin()+i,list.begin()+i+k);
     }
-    printf("%05d %d",list[0].address,list[0].data);
-    for(i=1;i<len;i++){
-        printf(" %05d\n%05d %d",list[i].address,list[i].address,list[i].data);
+    for(i=0;i<len;i++){
+        if(i==0) printf("%05d %d ",list[i].address,list[i].data);
+        else printf("%05d\n%05d %d ",list[i].address,list[i].address,list[i].data);
     }
-    printf(" -1");
+    printf("-1");
 }
