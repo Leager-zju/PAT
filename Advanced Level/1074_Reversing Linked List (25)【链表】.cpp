95fc74650d3ea7cd0ba65b2070f830cdea18a9ed
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-20 17:33:53 +0800

    Update 1074_Reversing Linked List (25)【链表】.cpp
    最后一段长度不够则不翻转

--- Advanced Level/1074_Reversing Linked List (25)【链表】.cpp
+++ Advanced Level/1074_Reversing Linked List (25)【链表】.cpp
@@ -9,13 +9,13 @@ struct info{
 
 int main(){
     int head,n,k;
-    int i,add;
+    int i,addr;
     vector<info> list;
     scanf("%d %d %d",&head,&n,&k);
     for(i=0;i<n;i++){
-        scanf("%d",&add);
-        scanf("%d %d",&l[add].data,&l[add].next);
-        l[add].address=add;
+        scanf("%d",&addr);
+        scanf("%d %d",&l[addr].data,&l[addr].next);
+        l[addr].address=addr;
     }
     if(head==-1){
         printf("-1");
@@ -26,9 +26,8 @@ int main(){
         head=l[head].next;
     }
     int len=list.size();
-    for(i=0;i<len;i+=k){
-        if(i+k>len) break;
-        else reverse(list.begin()+i,list.begin()+i+k);
+    for(i=0;i<=len-k;i+=k){
+        reverse(list.begin()+i,list.begin()+i+k);
     }
     printf("%05d %d",list[0].address,list[0].data);
     for(i=1;i<len;i++){
