21908b2f3406c51f023fea207715b65dd725f572
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-20 22:42:16 +0800

    Update 1052_Linked List Sorting (25)【链表】.cpp
    常规链表题

--- Advanced Level/1052_Linked List Sorting (25)【链表】.cpp
+++ Advanced Level/1052_Linked List Sorting (25)【链表】.cpp
@@ -1,4 +1,3 @@
-#include<iostream>
 #include<cstdio>
 #include<vector>
 #include<algorithm>
@@ -8,23 +7,17 @@ struct info{
     int address,data,next;
 }L[100010];
 
-vector<info> list;
 bool cmp(info a,info b){
     return a.data<b.data;
 }
 
 int main(){
-    int n,head,add,count=0;
-    cin>>n>>head;
-
-    if(head==-1){
-        cout<<"0 -1";
-        return 0;
-    }
-    
-    for(int i=0;i<n;i++){
-        cin>>add;
-        cin>>L[add].data>>L[add].next;
+    int n,i,head,add,count=0;
+    vector<info> list;
+    scanf("%d %d",&n,&head);
+    for(i=0;i<n;i++){
+        scanf("%d",&add);
+        scanf("%d %d",&L[add].data,&L[add].next);
         L[add].address=add;
     }
     while(head!=-1){
@@ -33,9 +26,9 @@ int main(){
         head=L[head].next;
     }
     sort(list.begin(),list.end(),cmp);
-    printf("%d %05d\n%05d %d",count,list[0].address,list[0].address,list[0].data);
-    for(int i=1;i<count;i++){
+    printf("%d",count);
+    for(i=0;i<count;i++){
         printf(" %05d\n%05d %d",list[i].address,list[i].address,list[i].data);
     }
-    cout<<" -1";
+    printf(" -1\n");
 }
