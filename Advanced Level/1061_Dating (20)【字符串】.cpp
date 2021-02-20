04c1dc1cb171749f35626182c4220943f63c9b4d
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-02-20 21:58:03 +0800

    Update 1061_Dating (20)【字符串】.cpp
    简单

--- Advanced Level/1061_Dating (20)【字符串】.cpp
+++ Advanced Level/1061_Dating (20)【字符串】.cpp
@@ -4,37 +4,31 @@
 using namespace std;
 
 int main(){
-    string s[7]={"MON","TUE","WED","THU","FRI","SAT","SUN"};
-    string a,b,c,d;
-    string ans;
-    getline(cin,a);
-    getline(cin,b);
-    getline(cin,c);
-    getline(cin,d);
-    int lena=a.length(),lenb=b.length(),lenc=c.length(),lend=d.length();
-    
-    int i,count=1;
-    for(i=0;i<min(lena,lenb);i++){
-        if(a[i]==b[i]){
-            if(count==1){
-                if(!(a[i]>='A' && a[i]<='G')) continue;
-                int d=a[i]-'A';
-                cout<<s[d]<<" ";
+    string s[7]={"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+    string a, b, c, d, ans;
+    cin >> a >> b >> c >> d;
+    int lena = a.length(), lenb = b.length(), lenc = c.length(), lend = d.length();
+    int i, count = 1;
+    for(i = 0; i < min(lena, lenb); i++){
+        if(a[i] == b[i]){
+            if(count == 1){
+                if(!(a[i] >= 'A' && a[i] <= 'G')) continue;
+                int d = a[i]-'A';
+                cout << s[d] << " ";
                 count++;
-            }
-            else if(count==2){
+            } else if(count == 2){
                 int m;
-                if(isdigit(a[i])) m=a[i]-'0';
-                else if(a[i]>='A'&&a[i]<='N') m=a[i]-'A'+10;
+                if(isdigit(a[i])) m = a[i]-'0';
+                else if(a[i] >= 'A' && a[i] <= 'N') m = a[i] - 'A' + 10;
                 else continue;
-                printf("%02d:",m);
+                printf("%02d:", m);
                 break;
             }
         }
     }
-    for(i=0;i<min(lenc,lend);i++){
-        if(c[i]==d[i] && isalpha(c[i])){
-            printf("%02d",i);
+    for(i = 0; i < min(lenc,lend); i++){
+        if(c[i] == d[i] && isalpha(c[i])){
+            printf("%02d", i);
             break;
         }
     }
