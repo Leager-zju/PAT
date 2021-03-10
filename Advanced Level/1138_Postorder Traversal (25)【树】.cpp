5c0939f3f1a8f0f4bf856a61eee8780c36378208
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-03-10 14:55:02 +0800

    Update 1138_Postorder Traversal (25)【树】.cpp
    若左子树存在，往左子树搜索；反之，若右子树存在，往右子树搜索；若左右均不存在，则找到目标结点，返回值。

--- Advanced Level/1138_Postorder Traversal (25)【树】.cpp
+++ Advanced Level/1138_Postorder Traversal (25)【树】.cpp
@@ -1,27 +1,28 @@
 #include<cstdio>
 #define maxn 50010
 
-int in[maxn],pre[maxn];
+int in[maxn], pre[maxn];
 
-int find(int l1,int r1,int l2,int r2){
-    if(l1==r1) return pre[l1];
-    int i,numofleft;
-    for(i=l2;i<=r2;i++){
-        if(in[i]==pre[l1]) break;
+int find(int l1, int r1, int l2, int r2){
+    if(l1 == r1) return pre[l1];
+    int i, numofleft;
+    for(i = l2; i <= r2; i++){
+        if(in[i] == pre[l1]) break;
     }
-    numofleft=i-l2;
-    if(numofleft) return find(l1+1,l1+numofleft,l2,i-1);
-    else return find(l1+numofleft+1,r1,i+1,r2);
+    numofleft = i-l2;
+    if(numofleft) return find(l1+1, l1+numofleft, l2, i-1);
+    else return find(l1+numofleft+1, r1, i+1, r2);
 }
 
 int main(){
-    int n,i;
-    scanf("%d",&n);
-    for(i=0;i<n;i++){
-        scanf("%d",&pre[i]);
+    int n, i;
+    scanf("%d", &n);
+    for(i = 0; i < n; i++){
+        scanf("%d", &pre[i]);
     }
-    for(i=0;i<n;i++){
-        scanf("%d",&in[i]);
+    for(i = 0; i < n; i++){
+        scanf("%d", &in[i]);
     }
-    printf("%d",find(0,n-1,0,n-1));
+    printf("%d", find(0, n-1, 0, n-1));
+    return 0;
 }
