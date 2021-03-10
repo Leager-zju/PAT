5b0f3cf3d8216347480d1cb91315dfc4248a943b
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-03-10 13:39:56 +0800

    Update 1155_Heap Paths (30)【二叉树】.cpp
    输入时更新标记ismax和ismin，遍历时先右子树再左子树。

--- Advanced Level/1155_Heap Paths (30)【二叉树】.cpp
+++ Advanced Level/1155_Heap Paths (30)【二叉树】.cpp
@@ -1,41 +1,43 @@
-#include<iostream>
 #include<vector>
+#include<algorithm>
+#include<iostream>
 using namespace std;
 
-
-int hsize;
-vector<int> heap,path;
-
-void travel(int root){
-    if(2*root>hsize){
-        for(int i=0;i<path.size();i++){
-            cout<<path[i];
-            if(i!=path.size()-1) cout<<" ";
-            else cout<<endl;
+vector<int> temp, heap;
+void dfs(int i){
+    if(2*i > heap.size()){
+        int n = temp.size();
+        for(int j = 0; j < n; j++){
+            cout << temp[j];
+            if(j < n-1) cout << " ";
+            else cout << endl;
         }
         return;
     }
-    for(int i=1;i>=0;i--){
-        if(2*root+i>hsize) continue;
-        path.push_back(heap[2*root+i]);
-        travel(2*root+i);
-        path.pop_back();
+    for(int j = 1; j >= 0; j--){
+        if(2*i+j < heap.size()){
+            temp.push_back(heap[2*i+j]);
+            dfs(2*i+j);
+            temp.pop_back();
+        }
     }
 }
-
 int main(){
-    int i;
-    int ismax=1,ismin=1;
-    cin>>hsize;
-    heap.resize(hsize+1);
-    for(i=1;i<=hsize;i++){
-        cin>>heap[i];
-        if(i>1 && heap[i]>heap[i/2]) ismax=0;
-        if(i>1 && heap[i]<heap[i/2]) ismin=0;
+    int n, i, ismax = 1, ismin = 1;
+
+    cin >> n;
+    heap.resize(n+1);
+    for(i = 1; i <= n; i++){
+        cin >> heap[i];
+        if(i > 1){
+            if(heap[i] > heap[i/2]) ismax = 0;
+            if(heap[i] < heap[i/2]) ismin = 0;
+        }
     }
-    path.push_back(heap[1]);
-    travel(1);
-    if(ismax)       cout<<"Max Heap"<<endl;
-    else if(ismin)  cout<<"Min Heap"<<endl;
-    else            cout<<"Not Heap"<<endl;
+    temp.push_back(heap[1]);
+    dfs(1);
+    if(ismax) cout << "Max Heap";
+    else if(ismin) cout << "Min Heap";
+    else cout << "Not Heap";
+    return 0;
 }
