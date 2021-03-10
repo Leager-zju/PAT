d8f99be9113b20eb94c830b9538320e479a3d595
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-03-10 19:28:49 +0800

    Update 1152_Google Recruitment (20)【字符串】.cpp
    判断素数。stoi()函数很好用。

--- Advanced Level/1152_Google Recruitment (20)【字符串】.cpp
+++ Advanced Level/1152_Google Recruitment (20)【字符串】.cpp
@@ -2,34 +2,24 @@
 #include<iostream>
 using namespace std;
 
-bool isprime(long long n){
-    if(n==0||n==1) return false;
-    for(int i=2;i*i<=n;i++){
-        if(n%i==0) return false;
+bool isprime(int n){
+    if(n == 0 || n == 1) return false;
+    for(int i = 2; i*i <= n; i++){
+        if(n % i == 0) return false;
     }
     return true;
 }
 
-long long str_int(string s){
-    long long ans=0;
-    for(int i=0;i<s.length();i++){
-        ans=ans*10+(s[i]-'0');
-    }
-    return ans;
-}
-
 int main(){
-    int L,k;
-    string s,subnum;
-    cin>>L>>k;
-    cin>>s;
-    for(i=0;i<=L-k;i++){
-        subnum=s.substr(i,k);
-        if(isprime(str_int(subnum))){
-            cout<<subnum<<endl;
+    int L, k, i;
+    string s;
+    cin >> L >> k >> s;
+    for(i = 0; i <= L-k; i++){
+        if(isprime(stoi(s.substr(i, k)))){
+            cout << s.substr(i, k) << endl;
             return 0;
         }
     }
-    cout<<"404"<<endl;
+    cout << "404" << endl;
     return 0;
 }
