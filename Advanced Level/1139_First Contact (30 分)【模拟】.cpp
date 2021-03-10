51d91e425db241efa4e62745bff23203cc82c9a9
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-03-10 14:59:48 +0800

    Update 1139_First Contact (30 分)【模拟】.cpp
    对给定的两个人，分别找他们的同性朋友，如果两个朋友还刚好认识，那整挺好，加入vector排序后输出即可。

--- Advanced Level/1139_First Contact (30 分)【模拟】.cpp
+++ Advanced Level/1139_First Contact (30 分)【模拟】.cpp
@@ -7,70 +7,69 @@ using namespace std;
 #define maxn 10010
 
 struct info{
-    int id,gender;
-}p1,p2,temp;
+    int id, gender = 1;
+}p1, p2, temp;
 
 vector<info> relation[maxn];
-bool G[maxn][maxn]={false};
+bool G[maxn][maxn] = {false};
 
-bool cmp(info &a,info &b){
-    if(a.id!=b.id) return a.id<b.id;
-    else return a.gender<b.gender;
+bool cmp(info &a, info &b){
+    if(a.id != b.id) return a.id < b.id;
+    else return a.gender < b.gender;
 }
 int strtonum(string s){
-    int i,ans=0;
-    for(i=0;i<s.length();i++){
-        ans=ans*10+(s[i]-'0');
+    int i, ans=0;
+    for(i = 0; i < s.length(); i++){
+        ans = ans*10 + (s[i]-'0');
     }
     return ans;
 }
 
 int main(){
-    int n,m,q,i,j;
-    string a,b;
-    scanf("%d %d",&n,&m);
+    int n, m, q, i, j;
+    string a, b;
+    scanf("%d %d", &n, &m);
     while(m--){
-        cin>>a>>b;
-        if(a[0]=='-'){
-            a=a.substr(1,4);
-            p1.gender=-1;
+        cin >> a >> b;
+        if(a[0] == '-'){
+            a = a.substr(1, 4);
+            p1.gender = -1;
         }
-        else p1.gender=1;
-        if(b[0]=='-'){
-            b=b.substr(1,4);
-            p2.gender=-1;
+        if(b[0] == '-'){
+            b=b.substr(1, 4);
+            p2.gender = -1;
         }
-        else p2.gender=1;
         
-        p1.id=strtonum(a);
-        p2.id=strtonum(b);
-        if(p1.gender==p2.gender){
+        p1.id = strtonum(a);
+        p2.id = strtonum(b);
+        if(p1.gender == p2.gender){
             relation[p1.id].push_back(p2);
             relation[p2.id].push_back(p1);
         }
-        G[p1.id][p2.id]=G[p2.id][p1.id]=true;
+        G[p1.id][p2.id] = G[p2.id][p1.id] = true;
     }
     
-    scanf("%d",&q);
+    scanf("%d", &q);
     while(q--){
         vector<info> ans;
-        cin>>a>>b;
-        if(a[0]=='-') a=a.substr(1,4);
-        if(b[0]=='-') b=b.substr(1,4);
-        n=strtonum(a);
-        m=strtonum(b);
+        cin >> a >> b;
+        if(a[0] == '-') a = a.substr(1, 4);
+        if(b[0] == '-') b = b.substr(1, 4);
+        n = strtonum(a);
+        m = strtonum(b);
         
-        for(i=0;i<relation[n].size();i++){
-            if(relation[n][i].id==m) continue;
-            for(j=0;j<relation[m].size();j++){
-                if(relation[m][j].id==n) continue;
-                if(G[relation[n][i].id][relation[m][j].id]) ans.push_back({relation[n][i].id,relation[m][j].id});
+        for(i = 0; i < relation[n].size(); i++){
+            if(relation[n][i].id == m) continue;
+            for(j = 0; j < relation[m].size(); j++){
+                if(relation[m][j].id == n) continue;
+                if(G[relation[n][i].id][relation[m][j].id]) ans.push_back({relation[n][i].id, relation[m][j].id});
             }
         }
-        printf("%d\n",(int)ans.size());
-        sort(ans.begin(),ans.end(),cmp);
-        for(i=0;i<ans.size();i++){
-            printf("%04d %04d\n",ans[i].id,ans[i].gender);
+        printf("%d\n", (int)ans.size());
+        sort(ans.begin(), ans.end(), cmp);
+        for(i = 0; i < ans.size(); i++){
+            printf("%04d %04d\n", ans[i].id, ans[i].gender);
         }
     }
+    return 0;
 }
