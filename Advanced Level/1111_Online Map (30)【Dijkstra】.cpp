cab70e369f3542155ad616e89fbfeff20d85ea36
竝 <63693262+Leager-zju@users.noreply.github.com>
2021-03-10 19:18:02 +0800

    Update 1111_Online Map (30)【Dijkstra】.cpp
    两次Dijkstra，这题会了其他Dij题也会了hh

--- Advanced Level/1111_Online Map (30)【Dijkstra】.cpp
+++ Advanced Level/1111_Online Map (30)【Dijkstra】.cpp
@@ -1,149 +1,124 @@
 #include<vector>
+#include<iostream>
 #include<algorithm>
-#include<cstdio>
 using namespace std;
+#define maxn 502
 #define inf 10e7
-#define maxn 520
 
-struct info{
-    int arrive,distance,t;
+struct node{
+    int end, len, time;
 };
 
-int dis[maxn],time[maxn],dispre[maxn];
-bool visited[maxn];
-vector<info> graph[maxn];
-vector<int> temppath,shortestpath,fastestpath,timepre[maxn];
+vector<bool> visit(maxn);
+vector<node> G[maxn];
+vector<int> temp, path1, path2, pre1(maxn), pre2[maxn];
+vector<int> L(maxn), T(maxn);
+int n, m;
 
-void findshortestpath(int s,int n){
-    fill(dis,dis+n,inf);
-    fill(time,time+n,inf);
-    fill(visited,visited+n,false);
-    dis[s]=time[s]=0;
-    int u,v,min,i,j;
-    for(i=0;i<n;i++){
-        u=-1,min=inf;
-        for(j=0;j<n;j++){
-            if(!visited[j] && dis[j]<min){
-                min=dis[j];
-                u=j;
+void Dij1(int start){
+    fill(visit.begin(), visit.end(), false);
+    fill(L.begin(), L.end(), inf);
+    fill(T.begin(), T.end(), inf);
+    L[start] = T[start] = 0;
+    for(int i = 0; i < n; i++){
+        int u = -1, min = inf;
+        for(int j = 0; j < n; j++){
+            if(!visit[j] && L[j] < min){
+                u = j;
+                min = L[j];
             }
         }
-        if(u==-1) return;
-        visited[u]=true;
-        for(j=0;j<graph[u].size();j++){
-            v=graph[u][j].arrive;
-            if(dis[u]+graph[u][j].distance<dis[v]){
-                dis[v]=dis[u]+graph[u][j].distance;
-                time[v]=time[u]+graph[u][j].t;
-                dispre[v]=u;
-            }
-            else if(dis[u]+graph[u][j].distance==dis[v] && time[u]+graph[u][j].t<time[v]){
-                time[v]=time[u]+graph[u][j].t;
-                dispre[v]=u;
+        if(u == -1) return;
+        visit[u] = true;
+        for(int j = 0; j < G[u].size(); j++){
+            int v = G[u][j].end;
+            if(L[v] > L[u] + G[u][j].len || (L[v] == L[u] + G[u][j].len && T[v] > T[u] + G[u][j].time)){
+                L[v] = L[u] + G[u][j].len;
+                T[v] = T[u] + G[u][j].time;
+                pre1[v] = u;
             }
         }
     }
 }
-
-void findfastestpath(int s,int n){
-    fill(dis,dis+n,inf);
-    fill(time,time+n,inf);
-    fill(visited,visited+n,false);
-    dis[s]=time[s]=0;
-    int i,j,u,v,min;
-    for(i=0;i<n;i++){
-        u=-1,min=inf;
-        for(int j=0;j<n;j++){
-            if(!visited[j] && time[j]<min){
-                min=time[j];
-                u=j;
+void Dij2(int start){
+    fill(visit.begin(), visit.end(), false);
+    fill(T.begin(), T.end(), inf);
+    T[start] = 0;
+    for(int i = 0; i < n; i++){
+        int u = -1, min = inf;
+        for(int j = 0; j < n; j++){
+            if(!visit[j] && T[j] < min){
+                u = j;
+                min = T[j];
             }
         }
-        if(u==-1) return;
-        visited[u]=true;
-        for(j=0;j<graph[u].size();j++){
-            v=graph[u][j].arrive;
-            if(time[u]+graph[u][j].t<time[v]){
-                time[v]=time[u]+graph[u][j].t;
-                timepre[v].clear();
-                timepre[v].push_back(u);
+        if(u == -1) return;
+        visit[u] = true;
+        for(int j = 0; j < G[u].size(); j++){
+            int v = G[u][j].end;
+            if(T[v] > T[u] + G[u][j].time){
+                T[v] = T[u] + G[u][j].time;
+                pre2[v].clear();
+                pre2[v].push_back(u);
             }
-            else if(time[u]+graph[u][j].t==time[v]) timepre[v].push_back(u);
+            else if(T[v] == T[u] + G[u][j].time) pre2[v].push_back(u);
         }
     }
 }
-
-void dispath(int u,int v){
-    while(u!=v){
-        shortestpath.push_back(u);
-        u=dispre[u];
-    }
-    shortestpath.push_back(v);
-}
-
-void timepath(int u,int v){
-    if(u==v){
-        if(fastestpath.size()==0 || temppath.size()<fastestpath.size()) fastestpath=temppath;
+void DFS1(int start, int end){
+    if(start == end){
+        path1 = temp;
         return;
     }
-    for(int i=0;i<timepre[u].size();i++){
-        temppath.push_back(timepre[u][i]);
-        timepath(timepre[u][i],v);
-        temppath.pop_back();
+    temp.push_back(pre1[end]);
+    DFS1(start, pre1[end]);
+    temp.pop_back();
+}
+void DFS2(int start, int end){
+    if(start == end){
+        if(path2.size() == 0 || temp.size() < path2.size()) path2 = temp;
+    }
+    for(int i = 0; i < pre2[end].size(); i++){
+        temp.push_back(pre2[end][i]);
+        DFS2(start, pre2[end][i]);
+        temp.pop_back();
     }
 }
 
 int main(){
-    int n,m,start,end;
-    int i,u,v,len,t,flag;
-    info a,b;
-    scanf("%d %d",&n,&m);
-    for(i=0;i<m;i++){
-        scanf("%d %d %d %d %d",&u,&v,&flag,&len,&t);
-        if(flag){
-            a.arrive=v;
-            a.distance=len;
-            a.t=t;
-            graph[u].push_back(a);
+    int i, u, v, tag, l, t;
+    cin >> n >> m;
+    for(i = 0; i < m; i++){
+        cin >> u >> v >> tag >> l >> t;
+        G[u].push_back({v, l, t});
+        if(tag == 0) G[v].push_back({u, l, t});
+    }
+    cin >> u >> v;
+    temp.push_back(v);
+    Dij1(u);
+    DFS1(u, v);
+    Dij2(u);
+    DFS2(u, v);
+    if(path1 == path2){
+        cout << "Distance = " << L[v] << "; Time = " << T[v] << ": ";
+        for(i = path1.size()-1; i >= 0; i--){
+            cout << path1[i];
+            if(i) cout << " -> ";
+            else cout << endl;
         }
-        else{
-            a.arrive=v;
-            b.arrive=u;
-            a.distance=b.distance=len;
-            a.t=b.t=t;
-            graph[u].push_back(a);
-            graph[v].push_back(b);
+    } else {
+        cout << "Distance = " << L[v] << ": ";
+        for(i = path1.size()-1; i >= 0; i--){
+            cout << path1[i];
+            if(i) cout << " -> ";
+            else cout << endl;
+        }
+        cout <<  "Time = " << T[v] << ": ";
+        for(i = path2.size()-1; i >= 0; i--){
+            cout << path2[i];
+            if(i) cout << " -> ";
+            else cout << endl;
         }
     }
-    scanf("%d %d",&start,&end);
-    
-    findshortestpath(start,n);
-    u=dis[end];
-    dispath(end,start);
-    
-    findfastestpath(start,n);
-    v=time[end];
-    temppath.push_back(end);
-    timepath(end,start);
-    
-    if(fastestpath==shortestpath){
-        printf("Distance = %d; Time = %d:",u,v);
-        for(i=shortestpath.size()-1;i>=0;i--){
-            printf(" %d",shortestpath[i]);
-            if(i!=0) printf(" ->");
-        }
-    }
-    else{
-        printf("Distance = %d:",u);
-        for(i=shortestpath.size()-1;i>=0;i--){
-            printf(" %d",shortestpath[i]);
-            if(i!=0) printf(" ->");
-        }
-        printf("\n");
-        printf("Time = %d:",v);
-        for(i=fastestpath.size()-1;i>=0;i--){
-            printf(" %d",fastestpath[i]);
-            if(i!=0) printf(" ->");
-        }
-    }
+    return 0;
 }
